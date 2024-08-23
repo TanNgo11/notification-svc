4967a7cb719e3b26783768b1339c59d777d00d42
TanNgo11 <tan.ngo.cit20@eiu.edu.vn>
2024-08-23 11:45:06 +0700

    feat/update-swagger-and-auditting-mongo

---
+++ src/main/java/com/shadcn/notificationservice/config/MongoAuditingConfig.java
@@ -0,0 +1,33 @@
+package com.shadcn.notificationservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+@Configuration
+@EnableMongoAuditing(auditorAwareRef = "auditorProvider")
+public class MongoAuditingConfig {
+
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        return new AuditorAwareImpl();
+    }
+
+    public static class AuditorAwareImpl implements AuditorAware<String> {
+
+        @Override
+        public Optional<String> getCurrentAuditor() {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            if (authentication == null || !authentication.isAuthenticated() || "anonymousUser".equals(authentication.getPrincipal())) {
+                return Optional.empty();
+            }
+            return Optional.ofNullable(authentication.getName());
+        }
+    }
+}
